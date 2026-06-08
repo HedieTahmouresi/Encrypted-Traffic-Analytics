e56c0b4f4f3d481204af8b2e3e24baccd89f30c2
HedieTahmouresi <tahmouresyhedie@gmail.com>
2026-06-08 05:12:59 +0330

    feat: preprocessor module finished using model pipelines

---
+++ src/inference_engine.py
@@ -0,0 +1,18 @@
+from preprocessor import ThreatPreprocessor
+
+class InferenceEngine:
+    def __init__(self, tier1_path, tier2_path):
+        self.edge_bouncer = ThreatPreprocessor(tier1_path)
+        self.gateway_analyzer = ThreatPreprocessor(tier2_path)
+
+    def analyze_flow(self, flow_df):
+        t1_input = self.edge_bouncer.sanitize_and_transform(flow_df)
+        is_anomaly = self.edge_bouncer.predict(t1_input)
+        
+        if is_anomaly[0] == 0:
+            return "BENIGN", None
+        
+        t2_input = self.gateway_analyzer.sanitize_and_transform(flow_df)
+        threat_type = self.gateway_analyzer.predict(t2_input)
+        
+        return "ANOMALY", threat_type
--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,38 +1,38 @@
-import os
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
 import joblib
+import pandas as pd
 
 class ThreatPreprocessor:
-    def __init__(self, features_csv_path, labels_csv_path, output_dir):
-        self.features_path = features_csv_path
-        self.labels_path = labels_csv_path
-        self.output_dir = output_dir
-        
-        os.makedirs(self.output_dir, exist_ok=True)
-        
-        self.scaler_path = os.path.join(output_dir, "tier1_scaler.pkl")
-        self.encoder_dict_path = os.path.join(output_dir, "categorical_encoders.pkl")
+    def __init__(self, pipeline_path, tier=1):
+        self.pipeline = joblib.load(pipeline_path)
+        self.expected_features = self.pipeline.named_steps['preprocessor'].get_feature_names_out()
+        self.tier = tier
 
-    def load_and_label_data(self):
-        print("[*] Loading extracted features...")
-        df_features = pd.read_csv(self.features_path)
+    def sanitize_and_transform(self, raw_df):
         
-        # TODO: Implement label merging logic
+        contaminants = []
+        if self.tier == 1:
+            contaminants = ['id', 'expiration_id', 'uid', 'src_ip', 'dst_ip', 'src_mac', 'dst_mac', 
+                'src_oui', 'dst_oui', 'vlan_id', 'tunnel_id', 'src_port', 'dst_port','datetime', 'ts', 'ts_ms', 'bidirectional_first_seen_ms', 'bidirectional_last_seen_ms',
+                'src2dst_first_seen_ms', 'src2dst_last_seen_ms', 'dst2src_first_seen_ms', 'dst2src_last_seen_ms','client_fingerprint', 
+                'server_fingerprint', 'requested_server_name', 'user_agent','application_name', 'application_category_name', 'application_is_guessed', 
+                'application_confidence', 'content_type', 'sni_domain','tls_version', 'cipher_suite', 'tls_resumed','http_requests', 'http_error_codes',
+                'ftp_user', 'ftp_failed_auths', 'ftp_success_auths',
+                'ssh_auth_success', 'ssh_auth_attempts', 'ssh_client', 'ssh_flows_past_60s', 'label'
+            ]
+        else:
+            contaminants = [
+                'label', 'id', 'uid', 'src_ip', 'dst_ip', 'src_mac', 'dst_mac', 
+                'src_oui', 'dst_oui', 'vlan_id', 'tunnel_id', 'src_port', 'dst_port',
+                'datetime', 'ts', 'ts_ms', 'bidirectional_first_seen_ms', 'bidirectional_last_seen_ms',
+                'src2dst_first_seen_ms', 'src2dst_last_seen_ms', 'dst2src_first_seen_ms', 'dst2src_last_seen_ms',
+                'client_fingerprint', 'server_fingerprint', 'requested_server_name', 'user_agent'
+            ]
+
         
-        return df_features
+        clean_df = raw_df.drop(columns=[c for c in contaminants if c in raw_df.columns], errors='ignore')
+        clean_df = clean_df.reindex(columns=self.expected_features, fill_value=0)
 
-    def encode_and_scale(self, df):
-        # TODO: Implement Scikit-Learn transformations
-        pass
+        return self.pipeline.named_steps['preprocessor'].transform(clean_df)
 
-    def prepare_for_training(self):
-        df_labeled = self.load_and_label_data()
-        # TODO: drop contaminated columns and handle missing values
-        # df_processed = self.encode_and_scale(df_labeled)
-        # return df_processed
-
-if __name__ == "__main__":
-    print("Initializing Preprocessor...")
+    def predict(self, clean_df):
+        return self.pipeline.named_steps['classifier'].predict(clean_df)
